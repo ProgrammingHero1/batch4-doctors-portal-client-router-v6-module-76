42cfc732c92e2992ee831a7edfdd77aee6f7aa4d
Programming Hero <programming.hero1@gmail.com>
2021-11-01 18:02:16 -0500

    appointment header layout

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,20 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import chair from '../../../images/chair.png';
+import Calendar from '../../Shared/Calendar/Calendar';
 
 const AppointmentHeader = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Calendar></Calendar>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{ width: '100%' }} src={chair} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
---
+++ src/Pages/Shared/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+
+const Calendar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Calendar;
