ea30d3671c125cbbc47928668006fa77dd97a583
Programming Hero <programming.hero1@gmail.com>
2021-10-31 21:14:58 -0500

    added single service

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const Service = () => {
+const Service = (props) => {
+    const { name, description, img } = props.service;
     return (
-        <div>
-
-        </div>
+        <Grid item xs={4} sm={4} md={4}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardMedia
+                    component="img"
+                    style={{ width: 'auto', height: '80px', margin: '0 auto' }}
+                    image={img}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Word of the Day
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="body2">
+                        {description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </Grid>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
+import Service from './../Service/Service';
+import fluoride from '../../../images/fluoride.png';
+import cavity from '../../../images/cavity.png';
+import whitening from '../../../images/whitening.png';
 
+const services = [
+    {
+        name: 'Fluoride Treatment',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: fluoride
+    },
+    {
+        name: 'Cavity Filling',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: cavity
+    },
+    {
+        name: 'Teeth Whitening',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: whitening
+    }
+]
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 
 const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {Array.from(Array(6)).map((_, index) => (
-                        <Grid item xs={4} sm={4} md={4} key={index}>
-                            <Item>xs=2</Item>
-                        </Grid>
-                    ))}
+                    {
+                        services.map(service => <Service
+                            key={service.name}
+                            service={service}
+                        ></Service>)
+                    }
                 </Grid>
             </Container>
         </Box>
