c6eec77b0127230c91ad020a414da449368710fb
Programming Hero <programming.hero1@gmail.com>
2021-10-31 23:05:30 -0500

    make image popup

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -3,25 +3,35 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import doctor from '../../../images/doctor.png'
 import bg from '../../../images/appointment-bg.png'
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
-const appointmentBg = {
-    background: `url(${bg})`
+const appointmentBanner = {
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45, 58, 74, 0.9)',
+    backgroundBlendMode: 'darken, luminosity',
+    marginTop: 175
 }
 
 const AppointmentBanner = () => {
     return (
-        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
                     <img
-                        style={{ width: 400, }}
+                        style={{ width: 400, marginTop: '-110px' }}
                         src={doctor} alt="" />
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <Typography variant="h6">
                         Appointment
                     </Typography>
+                    <Typography variant="h4">
+                        Make an Appointment Today
+                    </Typography>
+                    <Typography style={{ color: 'white' }} variant="h6" sx={{ fontSize: 16, mt: 5 }}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ab aperiam et!
+                    </Typography>
+                    <Button variant="contained" color="success">Learn More</Button>
                 </Grid>
             </Grid>
         </Box>
