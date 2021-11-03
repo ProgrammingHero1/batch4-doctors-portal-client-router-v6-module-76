36c987691c81269c3c9b6454da875d8d3a3a743a
Programming Hero <programming.hero1@gmail.com>
2021-11-02 22:11:58 -0500

    created useFirebase Hook

---
+++ src/Pages/Login/Login/Firebase/firebase.init.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './firebase.config';
+
+const initializeFirebase = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default initializeFirebase;
+
+/*
+steps for authentication
+----------------
+Step-1: Initial Setup
+1. firebase: create project
+2. create web app
+3. get configuration
+4. initialize firebase
+5. Enable auth method
+------------------
+Step-2: setup component
+1. Create Login Component
+2. Create Register Component
+3. Create Route for Login and Register
+------------------------
+Step 3: set auth system
+1. set up sign in method
+2. setup sign out method
+3. user state
+4. special observer
+5. return necessary methods and states from useFirebase
+---------------------
+Step 4: create auth context hook (useAuth)
+1. create a auth context
+2. Create context Provider
+3. set context Provider context value
+4. use Auth Provider
+5. create useAuth Hook
+---------------------
+Step 5: create private route
+1. create private Route
+2. set private route
+------------------
+Step-6: Redirect after login
+1. after login redirect user to their desired destination
+*/
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,55 @@
+import initializeFirebase from "../Pages/Login/Login/Firebase/firebase.init";
+import { useState, useEffect } from 'react';
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+
+
+// initialize firebase app
+initializeFirebase();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    const auth = getAuth();
+
+    const registerUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
+
+    // observer user state
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser({})
+            }
+        });
+        return () => unsubscribe;
+    }, [])
+
+    const logout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
+    return {
+        user,
+        registerUser,
+        logout,
+    }
+}
+
+export default useFirebase;
