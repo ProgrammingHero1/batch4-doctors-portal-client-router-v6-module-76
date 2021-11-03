c43c8e0e0f02f4bfa1a061f35ea6356a1c07b914
Programming Hero <programming.hero1@gmail.com>
2021-11-02 22:13:51 -0500

    typo

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -27,14 +27,14 @@ const useFirebase = () => {
 
     // observer user state
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setUser(user);
             } else {
                 setUser({})
             }
         });
-        return () => unsubscribe;
+        return () => unsubscribed;
     }, [])
 
     const logout = () => {
