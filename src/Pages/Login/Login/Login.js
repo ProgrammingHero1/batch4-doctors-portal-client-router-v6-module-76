31ea2bce94b8013bcd06c4d97c5ddf2407944974
Programming Hero <programming.hero1@gmail.com>
2021-11-02 20:04:37 -0500

    added login form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,44 @@
-import { Container } from '@mui/material';
-import React from 'react';
+import { Container, Typography, TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
 import { Grid } from '@mui/material';
 import login from '../../../images/login.png'
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({})
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+    const handleLoginSubmit = e => {
+        alert('hello')
+        e.preventDefault();
+    }
     return (
         <Container>
             <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-
+                <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>Login</Typography>
+                    <form onSubmit={handleLoginSubmit}>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Password"
+                            type="password"
+                            name="password"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                        <Button sx={{ width: '75%', m: 1 }} type="submit" variant="contained">Login</Button>
+                    </form>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <img style={{ width: '100%' }} src={login} alt="" />
