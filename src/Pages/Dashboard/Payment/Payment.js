f80d1e73a5f81568e00c8ca31a3c433752007420
Programming Hero <programming.hero1@gmail.com>
2021-11-13 16:13:42 -0600

    display payment specific data

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Payment = () => {
-    const { appointmentId } = useParams()
+    const { appointmentId } = useParams();
+    const [appointment, setAppointment] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/appointments/${appointmentId}`)
+            .then(res => res.json())
+            .then(data => setAppointment(data));
+    }, [appointmentId])
     return (
         <div>
-            <h2>Please Pay for: {appointmentId}</h2>
+            <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
+            <h4>Pay: ${appointment.price}</h4>
+
         </div>
     );
 };
